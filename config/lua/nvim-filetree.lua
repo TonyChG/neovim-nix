38f85f9f1b003856594389efbe2bc1d0d7d9b8ee
tonychg <tonychg7@pm.me>
2023-12-07 23:14:59 +0100

    feat(filetree): change width

--- config/lua/nvim-filetree.lua
+++ config/lua/nvim-filetree.lua
@@ -1,26 +1,26 @@
 require("nvim-tree").setup({
-  disable_netrw = true,
-  hijack_netrw = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  update_focused_file = {
-    enable = false,
-    update_root = false,
-    ignore_list = {},
-  },
-  auto_reload_on_write = true,
-  sync_root_with_cwd = true,
-  sort_by = "case_sensitive",
-  view = {
-    width = 40,
-    preserve_window_proportions = false,
-  },
-  renderer = {
-    group_empty = false,
-  },
-  filters = {
-    dotfiles = false,
-  },
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	update_focused_file = {
+		enable = false,
+		update_root = false,
+		ignore_list = {},
+	},
+	auto_reload_on_write = true,
+	sync_root_with_cwd = true,
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+		preserve_window_proportions = false,
+	},
+	renderer = {
+		group_empty = false,
+	},
+	filters = {
+		dotfiles = false,
+	},
 })
