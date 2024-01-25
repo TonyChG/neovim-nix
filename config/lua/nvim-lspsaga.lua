6fd593e10c61d99fe8459d4b26f0cc8bda40e1e1
tonychg <tonychg7@pm.me>
2024-01-25 13:12:53 +0100

    feat(nvim-lspsaga): remove breadcrumbs

--- config/lua/nvim-lspsaga.lua
+++ config/lua/nvim-lspsaga.lua
@@ -1,8 +1,11 @@
 require("lspsaga").setup({
-  rename = {
-    auto_save = true,
-  },
-  lightbulb = {
-    enable = false,
-  },
+	rename = {
+		auto_save = true,
+	},
+	lightbulb = {
+		enable = false,
+	},
+	symbol_in_winbar = {
+		enable = false,
+	},
 })
