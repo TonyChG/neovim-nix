90c9a21c683af69ba30263652235a15b043bd02f
tonychg <tonychg7@pm.me>
2024-04-10 23:25:54 +0200

    feat(filetree): increase width

--- config/lua/nvim-filetree.lua
+++ config/lua/nvim-filetree.lua
@@ -1,82 +1,82 @@
 require("nvim-tree").setup({
-  filters = {
-    dotfiles = false,
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  sync_root_with_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-    ignore_list = {},
-  },
-  view = {
-    adaptive_size = false,
-    side = "left",
-    width = 30,
-    preserve_window_proportions = true,
-  },
-  git = {
-    enable = true,
-    ignore = true,
-  },
-  filesystem_watchers = {
-    enable = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
-  },
-  renderer = {
-    root_folder_label = false,
-    highlight_git = true,
-    highlight_opened_files = "none",
-    group_empty = false,
+	filters = {
+		dotfiles = false,
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_cursor = true,
+	hijack_unnamed_buffer_when_opening = false,
+	sync_root_with_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+		ignore_list = {},
+	},
+	view = {
+		adaptive_size = false,
+		side = "left",
+		width = 40,
+		preserve_window_proportions = true,
+	},
+	git = {
+		enable = true,
+		ignore = true,
+	},
+	filesystem_watchers = {
+		enable = true,
+	},
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
+	},
+	renderer = {
+		root_folder_label = false,
+		highlight_git = true,
+		highlight_opened_files = "none",
+		group_empty = false,
 
-    indent_markers = {
-      enable = true,
-    },
+		indent_markers = {
+			enable = true,
+		},
 
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
 
-      glyphs = {
-        default = "󰈚",
-        symlink = "",
-        folder = {
-          default = "",
-          empty = "",
-          empty_open = "",
-          open = "",
-          symlink = "",
-          symlink_open = "",
-          arrow_open = "",
-          arrow_closed = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  auto_reload_on_write = true,
-  sort_by = "case_sensitive",
+			glyphs = {
+				default = "󰈚",
+				symlink = "",
+				folder = {
+					default = "",
+					empty = "",
+					empty_open = "",
+					open = "",
+					symlink = "",
+					symlink_open = "",
+					arrow_open = "",
+					arrow_closed = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	auto_reload_on_write = true,
+	sort_by = "case_sensitive",
 })
